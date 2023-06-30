3106a3643d3383732deecbc1e61133583af1bfe7
gmmendes <guilhermee.mendes@gmail.com>
2023-06-30 19:50:52 -0300

    feat: iniciando parte dois, vou criar uma branch nova

--- project-zombie/script.js
+++ project-zombie/script.js
@@ -1,5 +1,16 @@
+const getZombiesAndBullets = () => {
+  const zombiesValue = document.getElementById('zombies').value;
+  const bulletsValue = document.getElementById('bullets').value;
+
+  return { zombies: zombiesValue, bullets: bulletsValue };
+};
+
 const form = document.querySelector('form');
+
 form.addEventListener('click', (event) => {
   event.preventDefault();
+
+  const gameInfos = getZombiesAndBullets();
+
   window.location.href = './game.html';
 })
